22d73893d6047d94d0535b27963d777e1c7de459
Dust1920 <a218208885@gmail.com>
2024-08-14 12:06:41 -0700

    reestructure

--- process_data.py
+++ process_data.py
@@ -1,11 +1,32 @@
 import os
 import pandas as pd
+import zipfile
 
+# Folder de Origen de Archivos 
 folder_data = "data\\data_files"
+## Si no existe, creala. 
+if not os.path.exists(folder_data):
+    os.mkdir(folder_data)
+
+folder_rawdata = "data"
+files = os.listdir(folder_rawdata)
+
+zip_files = []
+for f in files:
+    if f.endswith(".zip"):
+        zip_files.append(f)
+
+idx = -1
+
+data_origin = zip_files[idx]
+
+path_data = os.path.join(folder_data, data_origin)
+with zipfile.ZipFile(path_data) as f:
+    f.extractall(folder_data)
+
+print(data_origin)
+
 files = os.listdir(folder_data)
-
-mode = 'sto'
-
 
 def get_data_mode(mode):
     files_mode = []
